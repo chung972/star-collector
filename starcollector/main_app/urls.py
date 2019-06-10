8493b6362fee29be91b4adef021a49f27cd07a2e
Albert Chung <chung972@gmail.com>
2019-06-10 17:17:45 -0500

    adds path for a star's detail page

--- starcollector/main_app/urls.py
+++ starcollector/main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('stars/', views.stars_index, name='index'),
+    path('stars/<int:star_id>/', views.stars_detail, name='detail'),
 ]

feddc296e6526485eca5f876a2623a05c42623a3
Albert Chung <chung972@gmail.com>
2019-06-11 13:15:02 -0500

    adds paths for creating, updating, and deleting stars

--- starcollector/main_app/urls.py
+++ starcollector/main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('stars/', views.stars_index, name='index'),
     path('stars/<int:star_id>/', views.stars_detail, name='detail'),
+    path('stars/create/', views.StarCreate.as_view(), name='stars_create'),
+    path('stars/<int:pk>/update/', views.StarUpdate.as_view(), name='stars_update'),
+    path('stars/<int:pk>/delete/', views.StarDelete.as_view(), name='stars_delete'),
 ]

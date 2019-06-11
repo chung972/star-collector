163eaa848854753138220a040d235c1ba19c2974
Albert Chung <chung972@gmail.com>
2019-06-11 13:12:28 -0500

    adds get_absolute_url method to Star model

--- starcollector/main_app/models.py
+++ starcollector/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,3 +12,6 @@ class Star(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'star_id': self.id})

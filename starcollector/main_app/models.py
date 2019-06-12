c99f6a76a650edbade9ede8e2d4edfb2635f6aa3
Albert Chung <chung972@gmail.com>
2019-06-12 13:39:05 -0500

    adds Viewing model; holds a foreign key on Star

--- starcollector/main_app/models.py
+++ starcollector/main_app/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+VISIBILITY = (
+    ('C', 'Clear'),
+    ('P', 'Partial'),
+    ('O', 'Obscured')
+)
 
 
 class Star(models.Model):
@@ -15,3 +20,24 @@ class Star(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'star_id': self.id})
+
+
+class Viewing(models.Model):
+    date = models.DateField("Viewing date")
+    time = models.TimeField(
+        blank=True,
+        null=True
+    )
+    visibility = models.CharField(
+        max_length=1,
+        choices=VISIBILITY,
+        default=VISIBILITY[0][0]
+    )
+
+    star = models.ForeignKey(Star, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_visibility_display()} on {self.date} at {self.time}"
+    
+    class Meta:
+        ordering = ['-date']

e0b5d8955dadfd496262e608d7801bbb35bd9435
Albert Chung <chung972@gmail.com>
2019-06-12 17:12:42 -0500

    adds routes for all Observatory CRUD operations

--- starcollector/main_app/urls.py
+++ starcollector/main_app/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('stars/<int:pk>/update/', views.StarUpdate.as_view(), name='stars_update'),
     path('stars/<int:pk>/delete/', views.StarDelete.as_view(), name='stars_delete'),
     path('stars/<int:star_id>/add_viewing/', views.add_viewing, name='add_viewing'),
+    path('observatories/', views.ObservatoryList.as_view(), name='observatories_index'),
+    path('observatories/<int:pk>/', views.ObservatoryDetail.as_view(), name='observatories_detail'),
+    path('observatories/create/', views.ObservatoryCreate.as_view(), name='observatories_create'),
+    path('observatories/<int:pk>/update/', views.ObservatoryUpdate.as_view(), name='observatories_update'),
+    path('observatories/<int:pk>/delete/', views.ObservatoryDelete.as_view(), name='observatories_delete'),
 ]

e78b6b0bbe04838e56e9c0a23b1cc8c81c6c9442
Albert Chung <chung972@gmail.com>
2019-06-12 13:42:57 -0500

    adds route for creating a viewing

--- starcollector/main_app/urls.py
+++ starcollector/main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('stars/create/', views.StarCreate.as_view(), name='stars_create'),
     path('stars/<int:pk>/update/', views.StarUpdate.as_view(), name='stars_update'),
     path('stars/<int:pk>/delete/', views.StarDelete.as_view(), name='stars_delete'),
+    path('stars/<int:star_id>/add_viewing/', views.add_viewing, name='add_viewing'),
 ]

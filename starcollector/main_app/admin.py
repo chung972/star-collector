ad8a9636c9a8e0f189d42099cba5d5a32be5a843
Albert Chung <chung972@gmail.com>
2019-06-12 17:11:05 -0500

    registers Observatory model with admin portal

--- starcollector/main_app/admin.py
+++ starcollector/main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Star, Viewing
+from .models import Star, Viewing, Observatory
 # Register your models here.
 admin.site.register(Star)
 admin.site.register(Viewing)
+admin.site.register(Observatory)

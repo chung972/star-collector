d9cf425cbf8b8c17c6423eb600f01526d70ef5a5
Albert Chung <chung972@gmail.com>
2019-06-15 14:03:11 -0500

    imports and registers Photo model

--- starcollector/main_app/admin.py
+++ starcollector/main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Star, Viewing, Observatory
+from .models import Star, Viewing, Observatory, Photo
 # Register your models here.
 admin.site.register(Star)
 admin.site.register(Viewing)
 admin.site.register(Observatory)
+admin.site.register(Photo)
